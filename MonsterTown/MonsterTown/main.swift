1ceb619dc7bc8bff90b11c98139c95590db0d375
Jigesh Parekh <JParekh117@gmail.com>
2016-05-08 09:41:52 -0700

    Master init for MonsterTown

---
+++ MonsterTown/MonsterTown/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MonsterTown
+//
+//  Created by Jigesh Parekh on 5/8/16.
+//  Copyright © 2016 Jigesh Parekh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
